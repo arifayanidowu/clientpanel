52aa0f5e66ba0b08e42c0a0d01fc5bc004a43051
stizzle123 <arifayanidowu2@yahoo.com>
2018-10-03 18:43:32 +0100

    Edit Client

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AppNavbar from "./components/layouts/AppNavbar";
 import Dashboard from "./components/layouts/Dashboard";
 import AddClient from "./components/clients/AddClient";
 import ClientDetails from "./components/clients/ClientDetails";
+import EditClient from "./components/clients/EditClient";
 
 import "./App.css";
 
@@ -21,6 +22,7 @@ class App extends Component {
                 <Route exact path="/" component={Dashboard} />
                 <Route exact path="/client/add" component={AddClient} />
                 <Route exact path="/client/:id" component={ClientDetails} />
+                <Route exact path="/client/edit/:id" component={EditClient} />
               </Switch>
             </div>
           </div>
